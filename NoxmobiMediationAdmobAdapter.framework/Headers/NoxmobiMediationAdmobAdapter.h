0f08bd4398f1e68ff99c26e27a792c226ceb80a2
nox_chong <gaochong@bignox.com>
2020-07-30 19:33:21 +0800

    第一版

---
+++ NoxmobiMediationAdmobAdapter.framework/Headers/NoxmobiMediationAdmobAdapter.h
@@ -0,0 +1,14 @@
+//
+//	NoxmobiMediationAdmobAdapter.h
+//	
+//	▄▄︻┻━━━┳━═━━   BUG~~
+//	
+//  Created by Chong on 2020/7/29.
+//  Copyright © 2020 Chong. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+#define NoxmobiMediationAdmobAdapterVersion @"7.63.0.0"
+
+
